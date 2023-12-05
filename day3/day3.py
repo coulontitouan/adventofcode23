23d7c2d86e5c7ecd16b2621a01a106a09d0ebb58
Livreur <coulontitouann@gmail.com>
2023-12-05 11:34:37 +0100

    day3 fini

--- day3/day3.py
+++ day3/day3.py
@@ -1,50 +1,50 @@
 print("-------------1ere partie-------------")
 
-# with open("input.txt", "r") as fichier:
-#     somme = 0
-#     y = 0
-#     dernier_chiffre = 0
-#     lignes = fichier.readlines()
-#     def construit_chiffre(x,y):
-#         chiffre = ""
-#         while x < 140 and lignes[y][x].isnumeric() :
-#             chiffre+=str(lignes[y][x])
-#             x+=1
-#         return chiffre
-#     def verif_symbole(x,y,longueur):
-#         verify = -1
-#         while verify < 2:
-#             verifx = -1
-#             while verifx < longueur+int(x<138):
-#                 if y+verify < 0:
-#                     verify+=1
-#                 if x+verifx < 0:
-#                     verifx+=1
-#                 if x+verifx > 139 or y+verify > 139:
-#                     break
-#                 symbole = lignes[y+verify][x+verifx]
-#                 if symbole != "." and not symbole.isnumeric():
-#                     return True
-#                 verifx+=1
-#             verify+=1
-#         return False
-#     while y < len(lignes):
-#         x = 0
-#         while x < len(lignes[y]):
-#             symbole =lignes[y][x]
-#             if symbole.isnumeric():
-#                 chiffre = int(construit_chiffre(x,y))
-#                 if verif_symbole(x,y,len(str(chiffre))):
-#                     somme+=chiffre
-#                 x+=len(str(chiffre))-1
-#             x+=1
-#         y+=1
+with open("input.txt", "r") as fichier:
+    somme = 0
+    y = 0
+    dernier_chiffre = 0
+    lignes = fichier.readlines()
+    def construit_chiffre(x,y):
+        chiffre = ""
+        while x < 140 and lignes[y][x].isnumeric() :
+            chiffre+=str(lignes[y][x])
+            x+=1
+        return chiffre
+    def verif_symbole(x,y,longueur):
+        verify = -1
+        while verify < 2:
+            verifx = -1
+            while verifx < longueur+int(x<138):
+                if y+verify < 0:
+                    verify+=1
+                if x+verifx < 0:
+                    verifx+=1
+                if x+verifx > 139 or y+verify > 139:
+                    break
+                symbole = lignes[y+verify][x+verifx]
+                if symbole != "." and not symbole.isnumeric():
+                    return True
+                verifx+=1
+            verify+=1
+        return False
+    while y < len(lignes):
+        x = 0
+        while x < len(lignes[y]):
+            symbole =lignes[y][x]
+            if symbole.isnumeric():
+                chiffre = int(construit_chiffre(x,y))
+                if verif_symbole(x,y,len(str(chiffre))):
+                    somme+=chiffre
+                x+=len(str(chiffre))-1
+            x+=1
+        y+=1
 
-#print(somme)
+print(somme)
 
 print("-------------2eme partie-------------")
 
-with open("/Users/livreur/Documents/GitHub/adventofcode23/day3/test.txt", "r") as fichier:
+with open("/Users/livreur/Documents/GitHub/adventofcode23/day3/input.txt", "r") as fichier:
     somme = 0
     y = 0
     lignes = fichier.readlines()
@@ -60,10 +60,10 @@ with open("/Users/livreur/Documents/GitHub/adventofcode23/day3/test.txt", "r") a
                 chiffre = f"{chiffre}{lignes[y][x+countplus]}"
                 countplus += 1
         except IndexError:
-            print("a")
+            pass
         return int(chiffre)
-    def ratio(x,y):
-        alentours = [[],[],[]]
+    def ratio(x,y,count):
+        alentours = set()
         verify = -1
         while verify < 2:
             verifx = -1
@@ -75,20 +75,24 @@ with open("/Users/livreur/Documents/GitHub/adventofcode23/day3/test.txt", "r") a
                 if x+verifx > 139 or y+verify > 139:
                     break
                 symbole = lignes[y+verify][x+verifx]
-                alentours[verify+1].append(symbole)
                 if symbole.isnumeric():
-                    print(construit_chiffre(x+verifx,y+verify))
+                    alentours.add(construit_chiffre(x+verifx,y+verify))
                 verifx+=1
             verify+=1
-        print("----")
-        for i in alentours:
-            print(i)
+        if len(alentours) == 2:
+            res = 1
+            for i in alentours:
+                res *=i
+            return res
+        return 0
+    count=0
     while y < len(lignes):
         x = 0
         while x < len(lignes[y]):
             symbole =lignes[y][x]
             if symbole == "*":
-                vratio = ratio(x,y)
+                somme += ratio(x,y,count)
+                count+=1
             x+=1
         y+=1
 
