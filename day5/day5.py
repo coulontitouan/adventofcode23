c9d3cfde4a069a48425c08c583776dd515c0b4e0
Livreur <coulontitouann@gmail.com>
2023-12-06 10:55:30 +0100

    day5 debut

--- day5/day5.py
+++ day5/day5.py
@@ -1,22 +1,17 @@
 print("-------------1ere partie-------------")
 
 with open("input.txt", "r") as fichier:
-    sum = 0
+    seeds = fichier.readlines()[0].replace("\n","").split(": ")[1].split(" ")
+    print(seeds)
+    locations = list()
     for ligne in fichier:
-        sum += int(f"{[i for i in ligne if i.isnumeric()][0]}{[i for i in reversed(ligne) if i.isnumeric()][0]}")
-print(sum)
+        pass
+print(min(locations))
 
 print("-------------2eme partie-------------")
 
 mots = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
 
-def remplace(chaine: str):
-    for mot in mots:
-        chaine = chaine.replace(mot, f"{mot[0]}{str(mots.index(mot)+1)}{mot[-1]}")
-    return chaine
-
 with open("input.txt", "r") as fichier:
-    sum = 0
     for ligne in fichier:
-        sum += int(f"{[i for i in remplace(ligne) if i.isnumeric()][0]}{[i for i in reversed(remplace(ligne)) if i.isnumeric()][0]}")
-print(sum)
+        pass
